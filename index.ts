b622a2e13ea40fbf03bdec232cd9ca2970493e85
Aaron Hatfield <aaron.hatfield@club-republic.com>
2026-01-02 14:23:59 +0000

    Hard Reset

--- index.ts
+++ index.ts
@@ -1,23 +1,18 @@
-import type { AstroIntegration } from 'astro';
+import type { AstroIntegration } from "astro";
 
-export default function createIntegration(): AstroIntegration {
-	// See the Integration API docs for full details
-	// https://docs.astro.build/en/reference/integrations-reference/
-	return {
-		name: '@example/my-integration',
-		hooks: {
-			'astro:config:setup': () => {
-				// See the @astrojs/react integration for an example
-				// https://github.com/withastro/astro/blob/main/packages/integrations/react/src/index.ts
-			},
-			'astro:build:setup': () => {
-				// See the @astrojs/react integration for an example
-				// https://github.com/withastro/astro/blob/main/packages/integrations/react/src/index.ts
-			},
-			'astro:build:done': () => {
-				// See the @astrojs/partytown integration for an example
-				// https://github.com/withastro/astro/blob/main/packages/integrations/partytown/src/index.ts
-			},
-		},
-	};
+export default function astroCookieBanner(): AstroIntegration {
+  return {
+    name: "astro-cookiebanner",
+    hooks: {
+      "astro:config:setup"() {
+        // will be used to initialise user files
+      },
+      "astro:build:setup"() {
+        // reserved for later if needed
+      },
+      "astro:build:done"() {
+        // reserved for later if needed
+      }
+    }
+  };
 }
